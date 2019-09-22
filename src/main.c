da1fd674ecdafa955d41f48140bbdb878895be4d
Lucas <lucaspaulom@hotmail.com>
2019-09-22 11:59:44 -0300

    feat: 🎸 Init
    Read informations from file

--- src/main.c
+++ src/main.c
@@ -5,5 +5,20 @@
 #include <string.h>
 
 int main(int argc, char const *argv[]) {
+	if ( argc != 2 ) /* argc should be 2 for ./a.out and filename.txt */ {
+        printf("usage: %s is provided,filename is not provided", argv[0]);
+	} else {
+        FILE *file = fopen(argv[1], "r");
+        if (!file) {
+            printf( "Could not open file\n");
+        }
+        else  {
+            int x;
+            while  ((x = fgetc(file)) != EOF) {
+                printf("%c", x);
+            }
+            fclose(file);
+        }
+    }
 	return 0; 
 }
